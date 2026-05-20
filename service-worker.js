552388fc99b1541b3edb555fd5511ec204b11f37
gtlgy-star <toughguy1234321@gmail.com>
2026-05-20 19:17:11 +0900

    5.20. 앱 완성본

---
+++ service-worker.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = "home-study-room-v1";
+
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./family-login.jpg",
+  "./app-icon-180.png",
+  "./app-icon-192.png",
+  "./app-icon-512.png",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const responseClone = response.clone();
+
+        if (request.url.startsWith(self.location.origin)) {
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(request, responseClone);
+          });
+        }
+
+        return response;
+      })
+      .catch(() => caches.match(request))
+  );
+});
